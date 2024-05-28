f94c1dc2d3cec9f4ea3bf89215d80df94a8e91db
mattarad <mttcannon@gmail.com>
2024-05-28 10:09:58 -0500

    Added functionality to Raffle.sol

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -22,33 +22,114 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
 /**
  * @title A sample Raffle Contract
  * @author Matt Cannon / Tutorial done by Patrick Collins and Cyfrin.io
  * @notice this contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughETHSent();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
 
+    /** Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    /** State Variables */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // @dev price required to be paid to enter lottory
     uint256 private immutable i_entranceFee;
+    // @dev duration of lottory in seconds
+    uint256 private immutable i_interval;
 
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callBackGasLimit;
+
+    VRFCoordinatorV2Interface immutable i_vrfCoordinator;
+
+    // @dev array of players that entered lottery, eligable to win lottery
     address payable[] private s_players;
+    address private s_recentWinner;
+
+    RaffleState private s_raffleState;
+
+    // @dev when the current round started, updated w/ each round
+    uint256 private s_lastTimestamp;
 
     /** Events */
     event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callBackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callBackGasLimit = callBackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimestamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughETHSent();
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    /**
+     *
+     */
+    function pickWinner() external {
+        // check if the lottory round is over
+        if ((block.timestamp - s_lastTimestamp) < i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callBackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) revert Raffle__TransferFailed();
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimestamp = block.timestamp;
+        emit PickedWinner(winner);
+    }
 
     /** Getter Functions */
     function getEntranceFee() public view returns (uint256) {
